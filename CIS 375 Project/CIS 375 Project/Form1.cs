2ca41b86ec5ca18ed0c2925a733e0e7ee1493d7a
Drew-Barlow <79282784+Drew-Barlow@users.noreply.github.com>
2022-04-25 00:32:28 -0400

    Basic GUI

---
+++ CIS 375 Project/CIS 375 Project/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CIS_375_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void getResultButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
